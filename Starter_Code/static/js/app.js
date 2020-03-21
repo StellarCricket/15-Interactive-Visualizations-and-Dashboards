4a5c2aca2b5e84b83dcc5d35df05b925f53976ed
Lyndsi <lyndsi.wolf@gmail.com>
2020-03-21 19:41:20 -0400

    Initial Push

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -1,20 +1,54 @@
+function navBoxFunction(sample) {
+var url = "samples.json";
+data = d3.json(url)
 
-// let string = JSON.stringify("url")
-// let obj = JSON.parse("string")
+d3.json(url).then(function(data){
+    var data = data.names;
+    var navBox  = d3.select('#selDataset');
+    navBox.html("");
+    Object.entries(data).forEach(([key, value])=>{
+        navBox.append("")
+        .text(`${key}:${value}`);
+    })
+    
+    var options = select
+        .selectAll('option')
+            .data(data).enter()
+            .append('option')
+            .text(function(d) {return d;});
+});
+
+function optionChanged(inputValue) {
+    console.log(inputValue)
+    d3.json(url).then (function(data){
+        var sample = data.samples;
+        var data = data.metadata;
+        console.log(data)
+        console.log(sample)
+        console.log(inputValue)
+        for(var i=0; i<data.length; i++){
+            console.log("ID:"+data[i].id);
+        }
+    });
+}
+
+if (data[i].id===parseInt(inputValue,10)){
+
+    console.log("Input Value"+inputValue);
+    d3.select("#sample-metadata")
+        .html("ID:"+data[i].id+"<br>"+"Ethnicity:"+data[i].ethnicity+"<br>"+"Gender:"+data[i].gender+"<br>"+"Age:"+data[i].age+"<br>"+"Location:"+data[i].location+"<br>"+"BB Type"+data[i].bbtype+"<br>"+"WFreq"+data[i].wfreq);
+}
+
 
 
 d3.json("samples.json").then((data)=> {
     
-   console.log(data)  
-    
+   console.log(data)      
 
-   var sampleValues = data.samples;
+    var sampleValues = data.samples;
 
-   var sortByValues = data.sort(function sortFunction(a,b) {
-       return b-a;
-
-     });
-    var slicedData = sortByValues.slice(0,10);
+  
+    var slicedData = sampleValues.slice(0,10);
     
     var reverseData = slicedData.reverse();
 
@@ -23,7 +57,7 @@ d3.json("samples.json").then((data)=> {
         y: reverseData.map(object => object.otu_ids),
         type: "bar",
         orientation: "h",
-        // text =  sample.samples.otu_values
+        text =  sample.samples.otu_values
 
     }
 
@@ -36,3 +70,4 @@ d3.json("samples.json").then((data)=> {
     };
     Plotly.newPlot("plot", data, layout);
 });
+}
