b0e0a4469027f0bb0cc49be497891b1104268a65
Lyndsi <lyndsi.wolf@gmail.com>
2020-04-19 18:36:55 -0400

    Updated JS File

--- Starter_Code/static/js/app.js
+++ Starter_Code/static/js/app.js
@@ -4,13 +4,8 @@ data = d3.json(url)
 
 d3.json(url).then(function(data){
     var data = data.names;
-    var navBox  = d3.select('#selDataset');
-    navBox.html("");
-    Object.entries(data).forEach(([key, value])=>{
-        navBox.append("")
-        .text(`${key}:${value}`);
-    })
-    
+    var select  = d3.select('#selDataset');
+
     var options = select
         .selectAll('option')
             .data(data).enter()
@@ -21,7 +16,7 @@ d3.json(url).then(function(data){
 function optionChanged(inputValue) {
     console.log(inputValue)
     d3.json(url).then (function(data){
-        var sample = data.samples;
+        var sample =  data.samples;
         var data = data.metadata;
         console.log(data)
         console.log(sample)
@@ -37,37 +32,47 @@ if (data[i].id===parseInt(inputValue,10)){
     console.log("Input Value"+inputValue);
     d3.select("#sample-metadata")
         .html("ID:"+data[i].id+"<br>"+"Ethnicity:"+data[i].ethnicity+"<br>"+"Gender:"+data[i].gender+"<br>"+"Age:"+data[i].age+"<br>"+"Location:"+data[i].location+"<br>"+"BB Type"+data[i].bbtype+"<br>"+"WFreq"+data[i].wfreq);
+
+        var otu_ids = sample[i].otu_ids
+        var sample_values = sample[i].sample_values
+        var otu_labels = sample[i].otu_labels
+        var allLabels=[];
+
+        var otuIDSlice = sample[i].otu_ids.slice(0,10)
+        var valueSlice = sample[i].sample_values.slice(0,10)
+        var labelSlice = sample[i].otu_labels.slice(0,10)
+        var otuLabels = [];
+        for (i=0;i<otu_ids.length;i++){
+            otuLabels.push('OTU:'+otus[i]);
+        }
+
+        var trace1 = {
+            y: otuLabels,
+            x: valueSlice,
+            type: "bar",
+            orientation: "h",
+            text: labelSlice,
+            marker: {
+                color: 'green'
+            }
+        };
+
+        var data = [trace1]
+
+        var layout = {
+            title: 'Operational Taxonomic Units by Volume',
+            width: 800,
+            showlegend: true, 
+            xaxis: {
+                tickangle: -45
+            },
+            
+        };
+        Plotly.newPlot("bar", data, layout);
+        
 }
 
 
+   
 
-d3.json("samples.json").then((data)=> {
-    
-   console.log(data)      
-
-    var sampleValues = data.samples;
-
-  
-    var slicedData = sampleValues.slice(0,10);
-    
-    var reverseData = slicedData.reverse();
-
-    var trace1 = {
-        x: reverseData.map(object => object.sampleValues),
-        y: reverseData.map(object => object.otu_ids),
-        type: "bar",
-        orientation: "h",
-        text =  sample.samples.otu_values
-
-    }
-
-    var data = [trace1];
-
-    var layout = {
-        title: "Belly Button Germs",
-        xaxis: { title: "X"}, 
-        yaxis: {title: "y"}
-    };
-    Plotly.newPlot("plot", data, layout);
-});
 }
